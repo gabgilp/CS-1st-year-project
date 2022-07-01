a14459d6958deb3b0b94ea5451fe26436032dfbe
gabgilp <gabgilp@gmail.com>
2022-07-01 16:34:52 +0200

    advanced3 might be done

--- obj/instructions_helper.d
+++
@@ -1,5 +0,0 @@
-obj/instructions_helper.o: src/instructions_helper.c \
- include/instructions_helper.h include/ijvm.h include/stack.h \
- include/basic_helper.h include/instructions_helper.h \
- include/frames_helper.h include/basic_helper.h include/stack.h \
- include/ijvm.h include/frames_helper.h
